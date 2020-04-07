de332a70b49a86bd5a6ecc96de0819ec554be3bf
Huy Tran <Nicudes@gmail.com>
2020-04-08 00:40:14 +0200

    newStaffPage

---
+++ HotellWhiteIsaac/HotellWhiteIsaac/Views/CleanerView/UnassignedPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotellWhiteIsaac.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HotellWhiteIsaac.Views.CleanerView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UnassignedPage : ContentPage
+    {
+        RoomsVM vm;
+        public UnassignedPage()
+        {
+            InitializeComponent();
+            vm = Resources["vm"] as RoomsVM; 
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            vm.ReadRooms();
+        }
+    }
+}
