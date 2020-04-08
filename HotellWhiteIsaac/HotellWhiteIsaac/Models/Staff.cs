9636dd30ea74ba921090d9ef34c21f7a88c14e30
Huy Tran <Nicudes@gmail.com>
2020-04-08 21:56:42 +0200

    new staff model

---
+++ HotellWhiteIsaac/HotellWhiteIsaac/Models/Staff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotellWhiteIsaac.Models
+{
+    class Staff
+    {
+        public int Id { get; set; } //Hämtar current user UID
+        public string Name { get; set; }    //Namnet på personen
+        public int DepartmentId { get; set; }   //I fields för att känna av vilken sida man ska hamna på
+  
+    }
+}
